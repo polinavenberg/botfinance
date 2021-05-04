4e4c71a85118acbc5a0bb224bc6565817f19b7fb
polinavenberg <venberg1976@gmail.com>
2021-05-04 21:13:54 +0300

    Functions for currency conversion

---
+++ conversion.py
@@ -0,0 +1,19 @@
+from pycbrf import ExchangeRates
+
+
+def convert(amount, currency_from, currency_to):
+    rates = ExchangeRates()
+    if currency_from == 'RUB':
+        from_rate = 1
+    else:
+        from_rate = rates[currency_from].value
+
+    if currency_to == 'RUB':
+        to_rate = 1
+    else:
+        to_rate = rates[currency_to].value
+
+    result = round(amount * from_rate / to_rate, 2)
+    result_message = str(result) + ' ' + currency_to
+    return result_message
+
