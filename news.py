27f60557698389ba53f15b6c0c728aa4f0188cd9
polinavenberg <venberg1976@gmail.com>
2021-05-04 21:16:20 +0300

    Added file with parsing functions

---
+++ news.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import globals
+import lxml
+
+
+def get_news():
+    response = requests.get(globals.url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    links = soup.find_all('a', class_='main__feed__link js-yandex-counter')
+    news = {}
+    for link in links:
+        news[link.text[2:]] = link['href']
+    return news
+
