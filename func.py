9090f644aa48c2fac00f4fe0b8111b80b3f4b488
polinavenberg <venberg1976@gmail.com>
2021-05-05 12:23:12 +0300

    flake8

---
+++ func.py
@@ -0,0 +1,85 @@
+from globals import bot
+import keyboards
+from globals import db
+import globals
+from conversion import convert
+import news
+
+
+def processing_get_text_mes(message):
+    message.text.lower()
+    if message.text == 'привет':
+        bot.send_message(message.chat.id, 'Привет!')
+    elif message.text == 'Новости' or message.text == '/news':
+        bot.send_message(message.chat.id,
+                         'Держи актуальные новости на сегодня:')
+        news_dict = news.get_news()
+        for key, value in news_dict.items():
+            bot.send_message(message.chat.id, value)
+    elif message.text == 'Конвертер валют' or message.text == '/conversion':
+        bot.send_message(message.chat.id, globals.convertion_message)
+    elif message.text == 'Подписаться на рассылку' or message.text == '/mailing':
+        bot.reply_to(message,
+                     'Выберите, на какую рассылку вы хотите подписаться',
+                     reply_markup=keyboards.subscription_keyboard)
+    elif message.text == 'Рассылка новостей':
+        bot.send_message(message.chat.id, 'Выберите действие:',
+                         reply_markup=keyboards.news_subscription_keyboard)
+    elif message.text == 'Подписаться на рассылку новостей':
+        if str(db.check_news_subscribtion(message.chat.id)) == '(False,)':
+            db.news_subscribe_unsubscribe(message.chat.id, True)
+            bot.send_message(message.chat.id,
+                             'Вы успешно подписались на рассылку новостей. '
+                             'Теперь каждый день в 10:00 вы будете получать '
+                             'актуальные новости.',
+                             reply_markup=keyboards.subscription_keyboard)
+        elif str(db.check_news_subscribtion(message.chat.id)) == '(True,)':
+            bot.send_message(message.chat.id,
+                             'Вы уже были подписаны на рассылку новостей',
+                             reply_markup=keyboards.subscription_keyboard)
+    elif message.text == 'Отписаться от рассылки новостей':
+        if str(db.check_news_subscribtion(message.chat.id)) == '(True,)':
+            db.news_subscribe_unsubscribe(message.chat.id, False)
+            bot.send_message(message.chat.id,
+                             'Вы успешно отписались от рассылки новостей',
+                             reply_markup=keyboards.subscription_keyboard)
+        else:
+            bot.send_message(message.chat.id,
+                             'Вы не были подписаны на рассылку новостей',
+                             reply_markup=keyboards.subscription_keyboard)
+    elif message.text == 'Рассылка курсов валют':
+        bot.send_message(message.chat.id, 'Выберите действие:',
+                         reply_markup=keyboards.currency_subscription_keyboard)
+    elif message.text == 'Подписаться на рассылку курсов валют':
+        if str(db.check_currency_subscribtion(message.chat.id)) == '(False,)':
+            db.currency_subscribe_unsubscribe(message.chat.id, True)
+            bot.send_message(message.chat.id,
+                             'Вы успешно подписались на рассылку курсов валют.'
+                             ' Теперь каждый день в 10:00 вы будете получать '
+                             'актуальные курсы валют.',
+                             reply_markup=keyboards.subscription_keyboard)
+        else:
+            bot.send_message(message.chat.id,
+                             'Вы уже были подписаны на рассылку курсов валют',
+                             reply_markup=keyboards.subscription_keyboard)
+    elif message.text == 'Отписаться от рассылки курсов валют':
+        if str(db.check_currency_subscribtion(message.chat.id)) == '(True,)':
+            db.currency_subscribe_unsubscribe(message.chat.id, False)
+            bot.send_message(message.chat.id,
+                             'Вы успешно отписались от рассылки курсов валют',
+                             reply_markup=keyboards.subscription_keyboard)
+        else:
+            bot.send_message(message.chat.id,
+                             'Вы не были подписаны на рассылку курсов валют',
+                             reply_markup=keyboards.subscription_keyboard)
+    elif message.text == 'Назад':
+        bot.send_message(message.chat.id, 'Возвращаемся в основное меню',
+                         reply_markup=keyboards.main_keyboard)
+    else:
+        try:
+            message_list = message.text.split(' ')
+            bot.send_message(message.chat.id,
+                             convert(int(message_list[0]), message_list[1],
+                                     message_list[2]))
+        except Exception:
+            bot.send_message(message.chat.id, 'Неправильный ввод')
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import schedule
 import time
 from conversion import convert
 from multiprocessing.context import Process
+from func import processing_get_text_mes
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -33,83 +34,10 @@ def get_text_message(message):
     Функция, которая отвечает на сообщения пользователя.
     :param message: сообщение пользователя
     :return:
+
     '''
-    message.text.lower()
-    if message.text == 'привет':
-        bot.send_message(message.chat.id, 'Привет!')
-    elif message.text == 'Новости' or message.text == '/news':
-        bot.send_message(message.chat.id,
-                         'Держи актуальные новости на сегодня:')
-        news_dict = news.get_news()
-        for key, value in news_dict.items():
-            bot.send_message(message.chat.id, value)
-    elif message.text == 'Конвертер валют' or message.text == '/conversion':
-        bot.send_message(message.chat.id, globals.convertion_message)
-    elif message.text == 'Подписаться на рассылку' or message.text == '/mailing':
-        bot.reply_to(message,
-                     'Выберите, на какую рассылку вы хотите подписаться',
-                     reply_markup=keyboards.subscription_keyboard)
-    elif message.text == 'Рассылка новостей':
-        bot.send_message(message.chat.id, 'Выберите действие:',
-                         reply_markup=keyboards.news_subscription_keyboard)
-    elif message.text == 'Подписаться на рассылку новостей':
-        if str(db.check_news_subscribtion(message.chat.id)) == '(False,)':
-            db.news_subscribe_unsubscribe(message.chat.id, True)
-            bot.send_message(message.chat.id,
-                             'Вы успешно подписались на рассылку новостей. '
-                             'Теперь каждый день в 10:00 вы будете получать '
-                             'актуальные новости.',
-                             reply_markup=keyboards.subscription_keyboard)
-        elif str(db.check_news_subscribtion(message.chat.id)) == '(True,)':
-            bot.send_message(message.chat.id,
-                             'Вы уже были подписаны на рассылку новостей',
-                             reply_markup=keyboards.subscription_keyboard)
-    elif message.text == 'Отписаться от рассылки новостей':
-        if str(db.check_news_subscribtion(message.chat.id)) == '(True,)':
-            db.news_subscribe_unsubscribe(message.chat.id, False)
-            bot.send_message(message.chat.id,
-                             'Вы успешно отписались от рассылки новостей',
-                             reply_markup=keyboards.subscription_keyboard)
-        else:
-            bot.send_message(message.chat.id,
-                             'Вы не были подписаны на рассылку новостей',
-                             reply_markup=keyboards.subscription_keyboard)
-    elif message.text == 'Рассылка курсов валют':
-        bot.send_message(message.chat.id, f'Выберите действие:',
-                         reply_markup=keyboards.currency_subscription_keyboard)
-    elif message.text == 'Подписаться на рассылку курсов валют':
-        if str(db.check_currency_subscribtion(message.chat.id)) == '(False,)':
-            db.currency_subscribe_unsubscribe(message.chat.id, True)
-            bot.send_message(message.chat.id,
-                             'Вы успешно подписались на рассылку курсов валют.'
-                             ' Теперь каждый день в 10:00 вы будете получать '
-                             'актуальные курсы валют.',
-                             reply_markup=keyboards.subscription_keyboard)
-        else:
-            bot.send_message(message.chat.id,
-                             'Вы уже были подписаны на рассылку курсов валют',
-                             reply_markup=keyboards.subscription_keyboard)
-    elif message.text == 'Отписаться от рассылки курсов валют':
-        if str(db.check_currency_subscribtion(message.chat.id)) == '(True,)':
-            db.currency_subscribe_unsubscribe(message.chat.id, False)
-            bot.send_message(message.chat.id,
-                             'Вы успешно отписались от рассылки курсов валют',
-                             reply_markup=keyboards.subscription_keyboard)
-        else:
-            bot.send_message(message.chat.id,
-                             'Вы не были подписаны на рассылку курсов валют',
-                             reply_markup=keyboards.subscription_keyboard)
-    elif message.text == 'Назад':
-        bot.send_message(message.chat.id, f'Возвращаемся в основное меню',
-                         reply_markup=keyboards.main_keyboard)
-    else:
-        try:
-            message_list = message.text.split(' ')
-            bot.send_message(message.chat.id,
-                             convert(int(message_list[0]), message_list[1],
-                                     message_list[2]))
-        except Exception:
-            bot.send_message(message.chat.id, 'Неправильный ввод')
+    processing_get_text_mes(message)
+
 
 
 schedule.every().day.at(globals.mailing_time).do(mailing.mailing_news)
