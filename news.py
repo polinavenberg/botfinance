130e0070256a6eec6e7f991b8d0b39ed09421220
polinavenberg <venberg1976@gmail.com>
2021-05-20 18:05:09 +0300

    Added comments

--- news.py
+++ news.py
@@ -9,10 +9,13 @@ def get_news():
     а значения -- ссылки на эти новости. Новости берутся с сайта rbc.ru
     :return: словарь с новостями
     '''
-    response = requests.get(globals.url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    response = requests.get(globals.url)  # отправляю запрос сайту
+    soup = BeautifulSoup(response.text, 'lxml')  # получаю http текст страницы
+    # в тексте страницы нахожу все заголовки и ссылки на актуальные новости, которые
+    # находятся в классе main__feed__link js-yandex-counter
     links = soup.find_all('a', class_='main__feed__link js-yandex-counter')
     news = {}
+    # создаю словарь и в его ключи записываю заголовки новостей, а в значения ссылки на новости
     for link in links:
-        news[link.text[2:]] = link['href']
+        news[link.text[globals.without_first_two_symbols:]] = link['href']
     return news
