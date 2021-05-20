e3e8eca6eb0d50fa5c52af0e5400ebd09e4589b5
polinavenberg <venberg1976@gmail.com>
2021-05-20 23:06:56 +0300

    Modified

--- news.py
+++ news.py
@@ -15,7 +15,8 @@ def get_news():
     # находятся в классе main__feed__link js-yandex-counter
     links = soup.find_all('a', class_='main__feed__link js-yandex-counter')
     # создаю словарь и в его ключи записываю заголовки новостей, а в значения ссылки на новости
-    news = {link.text[globals.without_first_two_symbols:]: link['href'] for link in links}
+    # первые два символа в каждом заголовке - это \n, записываем в словарь без них
+    news = {link.text[globals.without_first_two_enters:]: link['href'] for link in links}
     return news
 
 
